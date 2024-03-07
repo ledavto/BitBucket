e179957c09c5a359a05650eaf4ae709305fadf13
ledavto <ser_ega@ukr.net>
2024-03-07 12:14:20 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,14 +14,14 @@ import storage from 'redux-persist/lib/storage';
 // import { cartReducer } from './cart/cartSlice';
 import { farmReducer } from './farm/farmSlice';
 
-// const authPersistConfig = {
-//   key: 'auth',
-//   storage,
-//   whitelist: ['token'],
-// };
+const farmPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
-// const persistedReducer = persistReducer(authPersistConfig, farmReducer);
-const persistedReducer = persistReducer(farmReducer);
+const persistedReducer = persistReducer(authPersistConfig, farmReducer);
+// const persistedReducer = persistReducer(farmReducer);
 
 export const store = configureStore({
   reducer: { farm: farmReducer },
